d072cb762f150b5896250ed55b32035e13dcc1ca
acttq <acttq@gmail.com>
2024-01-11 22:40:39 +0800

    'store数据持久化'

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,17 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import './global.css'
 import App from './App'
 import { Provider } from 'react-redux'
-// import { PersistGate } from 'redux-persist/integration/react'
-import store from './store'
+import { PersistGate } from 'redux-persist/integration/react'
+import store, { persistor } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Router>
     </Provider>
   </React.StrictMode>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,32 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {configureStore,combineReducers} from "@reduxjs/toolkit";
+import { persistStore,persistReducer } from "redux-persist";
+// 选择持久化存储引擎，如localStorage或AsyncStorage
+import storage from 'redux-persist/lib/storage'
 import {userReducer} from './user/index.ts'
 
+
+const reducer = combineReducers({
+  user: userReducer
+})
+
+// 配置持久化设置
+const persistConfig = {
+  key: 'root', // 存储的键名
+  storage // 持久化存储引擎
+  // 可选的配置项，如白名单、黑名单等 选其一就好了
+  // blacklist:['test'], // 只有 test 不会被缓存
+  // whitelist: ["test"], // 只有 test 会被缓存
+}
+
+const persist = persistReducer(persistConfig,reducer)
 // 创建store 用来创建store对象，需要一个配置对象作为参数
 const store = configureStore({
-  reducer:{
-      user:userReducer,
-  }
+  reducer:persist,
+  middleware: (getDefaultMiddleware) =>  getDefaultMiddleware({
+    serializableCheck: false // 关闭默认的序列化检查//关闭严格模式
+  })
 });
+
+export const persistor = persistStore(store)
 
 export default store;
